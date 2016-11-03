8e757a080d2a8840831dd941fc7dea808df023bb
cashweaver <cashbweaver@gmail.com>
2016-11-03 11:52:58 -0700

    Write query results to file

--- src/query.js
+++ src/query.js
@@ -1,5 +1,7 @@
-const Promise = require('bluebird');
+const fs = require('fs');
 const math = require('mathjs');
+const path = require('path');
+const Promise = require('bluebird');
 const util = require('util');
 const colors = require('./colors');
 const commentController = require('./controllers/commentController');
@@ -31,7 +33,11 @@ logQueryConfiguration()
     const commentIds = results.map(result => result.id);
     const commentUrls = commentIds.map(id => `https://news.ycombinator.com/item?id=${id}`);
 
-    commentUrls.forEach((url) => {
-      console.log(url);
-    });
+    const resultsPath = path.resolve(`./${config.query.resultsName}`);
+    fs.writeFileSync(resultsPath, commentUrls.join('\n'));
+    console.log(colors.verbose(`Results written to ${resultsPath}`));
+
+    //commentUrls.forEach((url) => {
+      //console.log(url);
+    //});
   });
