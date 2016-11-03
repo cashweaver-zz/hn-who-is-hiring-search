43b3e3129d9de10df16331f26ae1a5741ef851be
cashweaver <cashbweaver@gmail.com>
2016-11-02 23:04:54 -0700

    Add query configuration

--- config.js
+++ config.js
@@ -5,7 +5,12 @@ module.exports = {
     password: 'password',
     path: `./db/${process.env.NODE_ENV}-hnwhoishiring.sqlite`,
   },
-  queryRegex: 'remote',
+  query: {
+    // Regex to test against Comment text
+    regex: '',
+    // Oldest Comment to include in the search (Unix time)
+    oldestComment: '',
+  },
   asyncWorkers: {
     askCheckingQ: 1,
     commentCreationQ: 10,
